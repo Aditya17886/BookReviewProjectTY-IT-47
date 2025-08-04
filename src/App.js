0a275a75e40fc60eede3bbe87a9949f49c952ee1
ADITYA PANIGRAHI <aditya.17886@sakec.ac.in>
2025-08-04 14:20:43 +0000

    push home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import "./styles.css";
+import React from "react";
+import Home from "./pages/Home";
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <Home />
     </div>
   );
 }
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <nav className="navbar">
+        <div className="logo">📚 ReadMate</div>
+        <ul>
+          <li>Home</li>
+          <li>Categories</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+
+      <div className="content">
+        <h1>
+          <span className="italic">Which</span> book{" "}
+          <span className="italic">do</span> you
+          <br />
+          want to <span className="italic">read?</span>
+        </h1>
+        <p>
+          Lost in choices? Explore our handpicked collection and find your next
+          great read!
+        </p>
+        <button className="explore-btn">
+          Explore <span className="arrow">→</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
